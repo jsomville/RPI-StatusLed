c5850db1abe55c3ade2decc000561b703ee4c1b4
jsomville <42740767+jsomville@users.noreply.github.com>
2021-04-07 19:37:00 +0200

    add variable for blink delay

--- statusled.py
+++ statusled.py
@@ -1,15 +1,16 @@
+#!/usr/bin/env python3
+
 import RPi.GPIO as GPIO
 from time import sleep
 
 LED_PIN=40
+BLINK_DELAY=1
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)
 
 while True:
-	GPIO.output(LED_PIN, GPIO.HIGH)
-	sleep(1)
-	GPIO.output(LED_PIN, GPIO.LOW)
-	sleep(1)
-
-
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    sleep(BLINK_DELAY)
+    GPIO.output(LED_PIN, GPIO.LOW)
+    sleep(BLINK_DELAY)
