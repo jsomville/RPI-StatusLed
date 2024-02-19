58f0fbed2eb8cfc43a64600abf8b9412ed9859dd
jsomville <42740767+jsomville@users.noreply.github.com>
2024-02-19 14:19:01 +0100

    Update statusled.py

--- statusled.py
+++ statusled.py
@@ -4,12 +4,16 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 LED_PIN=40
-BLINK_DELAY=1
+BLINK_DELAY=1 #In seconds
 
+#Setup GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)
 
+#Main Loop
 while True:
+    
+    #Blink the Led
     GPIO.output(LED_PIN, GPIO.HIGH)
     sleep(BLINK_DELAY)
     GPIO.output(LED_PIN, GPIO.LOW)
